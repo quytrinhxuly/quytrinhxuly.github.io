c56fd021eee0d4af802537f290c8e5a4e0256dc3
Tu Van <tu.van@stepmediasoftware.com>
2024-09-05 18:29:32 +0700

    add router

--- my-reactjs-app/src/App.js
+++ my-reactjs-app/src/App.js
@@ -1,10 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import "./App.css";
-import QuyTrinhXuLyPage from "./pages/quy-trinh-xu-ly/index"
+import Login from "./pages/login";
+import QuyTrinhXuLyPage from "./pages/quy-trinh-xu-ly/index";
 
 function App() {
   return (
     <div className="App">
-      <QuyTrinhXuLyPage />
+      <Router>
+        <Routes>
+          <Route path="/" element={<QuyTrinhXuLyPage />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
