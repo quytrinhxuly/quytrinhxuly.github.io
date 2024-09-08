cf957d0d2f5eea03e63fbbaffbc699d866f57920
Tu Van <tu.van@stepmediasoftware.com>
2024-09-08 13:50:11 +0700

    Implement API get list

--- my-reactjs-app/back-end-gs/telegram.service.js
+++ my-reactjs-app/back-end-gs/telegram.service.js
@@ -1,37 +1,42 @@
 class TelegramService {
-    constructor(chatId, botToken) {
-      this.chatId = chatId;
-      this.botToken = botToken;
-    }
-  
-    sendMessage(message) {
-      const payload = {
-        method: "sendMessage",
-        chat_id: this.chatId, // Thay YOUR_CHAT_ID bằng ID của nhóm Telegram của bạn
-        text: message,
-      };
-      const options = {
-        method: "post",
-        payload: payload,
-      };
-      UrlFetchApp.fetch(`https://api.telegram.org/bot${this.botToken}/sendMessage`, options);
-    }
-  
-    sendMessageWithInlineKeyboard(message, buttons) {
-      const keyboard = {
-        inline_keyboard: [buttons],
-      };
-      const payload = {
-        method: "sendMessage",
-        chat_id: this.chatId, // Thay YOUR_CHAT_ID bằng ID của nhóm Telegram của bạn
-        text: message,
-        reply_markup: JSON.stringify(keyboard),
-      };
-      const options = {
-        method: "post",
-        payload: payload,
-      };
-      UrlFetchApp.fetch(`https://api.telegram.org/bot${this.botToken}/sendMessage`, options);
-    }
+  constructor(chatId, botToken) {
+    this.chatId = chatId;
+    this.botToken = botToken;
   }
-  
+
+  sendMessage(message) {
+    const payload = {
+      method: "sendMessage",
+      chat_id: this.chatId, // Thay YOUR_CHAT_ID bằng ID của nhóm Telegram của bạn
+      text: message,
+    };
+    const options = {
+      method: "post",
+      payload: payload,
+    };
+    UrlFetchApp.fetch(
+      `https://api.telegram.org/bot${this.botToken}/sendMessage`,
+      options
+    );
+  }
+
+  sendMessageWithInlineKeyboard(message, buttons) {
+    const keyboard = {
+      inline_keyboard: [buttons],
+    };
+    const payload = {
+      method: "sendMessage",
+      chat_id: this.chatId, // Thay YOUR_CHAT_ID bằng ID của nhóm Telegram của bạn
+      text: message,
+      reply_markup: JSON.stringify(keyboard),
+    };
+    const options = {
+      method: "post",
+      payload: payload,
+    };
+    UrlFetchApp.fetch(
+      `https://api.telegram.org/bot${this.botToken}/sendMessage`,
+      options
+    );
+  }
+}
